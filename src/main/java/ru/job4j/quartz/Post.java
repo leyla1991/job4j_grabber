4f6b69e4662e2a2ab663b78cc0223ae2391ee536
leyla1991 <atagleyla@mail.ru>
2023-04-17 15:43:52 +0300

    2.2. Модель данных - Post. [#285211]

---
+++ src/main/java/ru/job4j/quartz/Post.java
@@ -0,0 +1,50 @@
+package ru.job4j.quartz;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class Post {
+
+    private int id;
+
+    private String title;
+
+    private String link;
+
+     private String description;
+
+     private LocalDateTime create;
+
+
+     public Post(int id, String title, String link, String description, LocalDateTime create) {
+         this.id = id;
+         this.title = title;
+         this.link = link;
+         this.description = description;
+         this.create = create;
+     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Post post = (Post) o;
+        return id == post.id && link.equals(post.link);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, link);
+    }
+
+    @Override
+    public String toString() {
+        return "Post{"
+                + "id=" + id
+                + ", title='" + title + '\''
+                + ", link='" + link + '\''
+                + ", description='" + description + '\''
+                + ", create=" + create
+                + '}';
+    }
+}
